059a2d6735936f2200af2936d8992179bd504487
SaqibAK001 <saqibak045@gmail.com>
2026-06-17 23:49:30 +0530

    asd

--- source code/frontend/src/services/api.js
+++ source code/frontend/src/services/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+// Train RL model
+export const trainRL = (envMode = "static") => {
+  return API.post("/train", {
+    env_mode: envMode,
+  });
+};
+
+// Run all algorithms
+export const runAllAlgorithms = (payload) => {
+  return API.post("/run-all", payload);
+};
 
 export default API;
